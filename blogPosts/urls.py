1b26c8ef4a9b0b1e42f6add3e4d637ad125e7ab4
seoseongq <sskpilot-_-@snu.ac.kr>
2021-07-20 01:25:08 +0900

    jaebin sarang

--- blogPosts/urls.py
+++ blogPosts/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
     path('new/', views.new, name= 'new'),
     path('<int:id>/', views.show, name= 'show'), 
     path('<int:id>/delete', views.delete, name= 'delete'), 
-    path('<int:id>/update', views.update, name= 'update'), 
+    #path('<int:id>/update', views.update, name= 'update'), 
     path('accounts/', include('accounts.urls')),
-    path('text1/', views.text1, name = "text1"),
-    path('text2/', views.text2, name = "text2"),
-    path('text3/', views.text3, name = "text3"),
+    path('text1/<int:id>', views.text1, name = "text1"),
+    path('text2/<int:id>', views.update, name = "update"),
+    path('text3/<int:id>', views.text3, name = "text3"),
 ]
 
 
