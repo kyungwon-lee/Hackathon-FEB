6ed2eeb7f6b5f2f3e2f90a89ee04e427042fc761
kyungwon-lee <kw375310@gmail.com>
2021-07-24 06:01:56 +0900

    [feat] Final Commit

--- FEB/settings.py
+++ FEB/settings.py
@@ -107,6 +107,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -210,3 +211,7 @@ X_FRAME_OPTIONS = 'ALLOWALL'
 XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
